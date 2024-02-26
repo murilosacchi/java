88026b2ea729affc6d63855406b735cde68f06ba
Murilo Sacchi <144244138+murilosacchi@users.noreply.github.com>
2024-02-26 18:49:42 -0300

    Create SomaDoisNumeros.java

---
+++ soma/SomaDoisNumeros.java
@@ -0,0 +1,15 @@
+import javax.swing. JOptionPane;
+public class SomaDoisNumeros {
+    public static void main(String[] args) {
+        double primeiroValor;
+        double segundoValor;
+        double resultado;
+        primeiroValor =
+        Double.parseDouble(JOptionPane.showInputDialog("Digite o primeiro valor:"));
+        segundoValor = 
+        Double.parseDouble(JOptionPane.showInputDialog("Digite o segundo valor:"));
+        resultado = primeiroValor + segundoValor;
+        //JOptionPane.showMessageDialog(null, resultado);
+        JOptionPane.showMessageDialog(null, primeiroValor + " + " + segundoValor + " = " + resultado);
+    } 
+}
