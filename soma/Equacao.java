03f75f9538a92bdf4875431d95abc5ba5182eb84
Murilo Sacchi <144244138+murilosacchi@users.noreply.github.com>
2024-02-28 19:13:14 -0300

    Update Equacao.java

--- soma/Equacao.java
+++ soma/Equacao.java
@@ -1,30 +1,27 @@
 import javax.swing.JOptionPane;
 
-public class SomaDoisNumeros {
+public class equacao {
     public static void main(String[] args) {
 
-        //declaração de variaveis
+        // declaração de variaveis
         double primeiroValor;
         double segundoValor;
         double terceiroValor;
         double quartoValor;
         double resultado;
 
-        //entrada
-        primeiroValor =
-        Double.parseDouble(JOptionPane.showInputDialog(""));
-        segundoValor = 
-        Double.parseDouble(JOptionPane.showInputDialog(""));
-        terceiroValor = 
-        Double.parseDouble(JOptionPane.showInputDialog(""));
-        quartoValor = 
-        Double.parseDouble(JOptionPane.showInputDialog(""));
+        // entrada
+        primeiroValor = Double.parseDouble(JOptionPane.showInputDialog("Digite um valor"));
+        segundoValor = Double.parseDouble(JOptionPane.showInputDialog("Digite outro valor"));
+        terceiroValor = Double.parseDouble(JOptionPane.showInputDialog("Digite outro valor"));
+        quartoValor = Double.parseDouble(JOptionPane.showInputDialog("Digite outro valor"));
 
-        //processamento
-        resultado = (primeiroValor*primeiroValor)+(segundoValor*s);
+        // processamento
+        resultado = (primeiroValor * primeiroValor) + (segundoValor * segundoValor) + (terceiroValor) * (terceiroValor)
+                + (quartoValor) * (quartoValor);
 
-        //saída
-        //JOptionPane.showMessageDialog(null, resultado);
-        JOptionPane.showMessageDialog(null, "Voce possui " + resultado + " dolares, que corresponde a " + segundoValor + " reais");
+        // saída
+        // JOptionPane.showMessageDialog(null, resultado);
+        JOptionPane.showMessageDialog(null, "A soma dos quadrados é " + resultado);
     }
 }
