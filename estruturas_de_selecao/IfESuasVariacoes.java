2f68ebcf85a8c2881e15e760345b4853301a9794
Murilo Sacchi <murilo.sacchi1126@gmail.com>
2024-03-06 12:54:37 -0300

    Create IfESuasVariacoes.java

---
+++ estruturas_de_selecao/IfESuasVariacoes.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class IfESuasVariacoes {
+    public static void main(String[] args) {
+        var leitor = new Scanner(System.in);
+        final int VALOR_MINIMO = 0, VALOR_MAXIMO = 10;
+        System.out.println("Qual a sua nota?");
+        var nota = leitor.nextDouble();
+        // if (nota >= 6) {
+        // System.out.println("Aprovado");
+        // System.out.println("Parabéns");
+        // }
+        // else
+        // System.out.println("De recuperação");
+        // System.out.println("Até logo");
+
+        if (nota >= VALOR_MINIMO && nota <= VALOR_MAXIMO) {
+            if (nota >= 9) {
+                System.out.println("A");
+                System.out.println("Parabéns");
+
+            } else if (nota >= 6) {
+                System.out.println("C");
+
+            } else {
+                System.out.println("De recuperação");
+            }
+
+        } else {
+            System.out.println("Nota inválida");
+        }
+
+        leitor.close();
+
+    }
+}
