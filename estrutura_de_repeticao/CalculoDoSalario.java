26e155fffddd03e121ad6f2d26e915696e37f523
Murilo Sacchi <murilo.sacchi1126@gmail.com>
2024-03-21 12:54:49 -0300

    new

--- estrutura_de_repeticao/CalculoDoSalario.java
+++ estrutura_de_repeticao/CalculoDoSalario.java
@@ -3,43 +3,58 @@ import java.util.Scanner;
 public class CalculoDoSalario {
     public static void main(String[] args) {
         var leitor = new Scanner(System.in);
-        int funcionarios;
-        double salarioMinimo;
-        double horas;
-        String categoria;
-        String turno;
 
+        // funcionarios e salario mininmo
         System.out.println("Digite o número de funcionários: ");
-        funcionarios = leitor.nextInt();
-        System.out.println("Digite o valor do salário mínimo: ");
-        salarioMinimo = leitor.nextDouble();
+        int funcionarios = leitor.nextInt();
+        System.out.println("Digite o valor do salário mínimo");
+        double salarioMinimo = leitor.nextDouble();
 
-        for (int i = 1; i <= funcionarios;) {
-            System.out.println("Digite quantas horas foram trabalhadas");
-            horas = leitor.nextDouble();
-            leitor.nextLine();
-            System.out.println("Digite a categoria");
-            categoria = leitor.nextLine();
-            System.out.println("Digite o turno");
-            turno = leitor.nextLine();
+        // for para cada funcionario
+        for (int i = 0; i < funcionarios; i++) {
 
+            // numero de horas trabalhadas
+            int horasTrabalhadas;
             do {
-                do {
-                    do {
-                        if (categoria == "G") {
-                            if (turno == "N") {
-                                
-                            }
-                            else if (turno == "M" || turno == "V") {
-                                
-                            }                            
-                        }
-                    } while (turno == "N" || turno == "M" || turno == "V");
-                    
-                } while (categoria == "G" || categoria == "O");
-            } while (horas >= 0);
-            i++;
-        }
+                System.out.println("Digite o número de horas trabalhadas do funcionário " + (i + 1) + ": ");
+                horasTrabalhadas = leitor.nextInt();
+            } while (horasTrabalhadas <= 0);
 
+            // categoria
+            String categoria;
+            do {
+                System.out.println("Digite a categoria: ");
+                categoria = leitor.next().toUpperCase();
+            } while (!"GO".contains(categoria));
+
+            // turno
+            String turno;
+            do {
+                System.out.println("Digite o turno: ");
+                turno = leitor.next().toUpperCase();
+            } while (!"MNV".contains(turno));
+
+            // valor hora trabalhada
+            if (categoria.contains("G")) {
+                if (turno.contains("N")) {
+                    double salario = salarioMinimo * 0.18;
+                    System.out.println("O valor da hora trabalhada é R$" + salario);
+                } 
+                else {
+                    double salario = salarioMinimo * 0.15;
+                    System.out.println("O valor da hora trabalhada é R$" + salario);
+                }
+            }
+            else {
+                if (turno.contains("N")) {
+                    double salario = salarioMinimo * 0.13;
+                    System.out.println("O valor da hora trabalhada é R$" + salario); 
+                }
+                else {
+                    double salario = salarioMinimo * 0.10;
+                    System.out.println("O valor da hora trabalhada é R$" + salario);
+                }
+            }
+        }
     }
 }
--- estrutura_de_repeticao/anotacoes.java
+++
@@ -1,7 +0,0 @@
-//1. pedir o numero de funcionarios e o valor do salario minimo 
-//2. fazer um for que executa n vezes, para cada vez 
-//3. pegar numero de horas trabalhadas (validar com do while, tem que ser maior que zero)
-//   turno 
-//   pegar categoria 
-//
-//4. mostrar o valor da hora trabalhada de acordo com a tabela
