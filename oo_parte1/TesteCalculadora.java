3f197442088ea2444724d1fd3e12ca970c983ab9
Murilo Sacchi <murilo.sacchi1126@gmail.com>
2024-03-27 12:53:22 -0300

    add

---
+++ oo_parte1/TesteCalculadora.java
@@ -0,0 +1,9 @@
+public class TesteCalculadora {
+    public static void main(String[] args) {
+        System.out.println("Começando...");
+        Calculadora calc = new Calculadora();
+        int resultado = calc.somar(5, 4);
+        System.out.println(resultado);
+        System.out.println("Terminando...");
+    }    
+}
