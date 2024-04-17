165df3de1c66068521d1e7ddc05c890a7b00ccfe
Murilo Sacchi <144244138+murilosacchi@users.noreply.github.com>
2024-04-17 12:51:36 -0300

    new

---
+++ interface_grafica/20241_maua_tti101_t1_sistema_academico/src/main/java/br/maua/_maua_tti101_t1_sistema_academico/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.maua._maua_tti101_t1_sistema_academico;
+
+/**
+ *
+ * @author 23.10260-8
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
